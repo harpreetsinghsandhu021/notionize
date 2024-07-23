39266677531bed7200798e2576076a9232d259bf
Harpreet Singh <harpreetsinghsandhu021@gmail.com>
2024-07-23 23:15:03 +0530

    fix - changed auth config

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://intimate-piglet-61.clerk.accounts.dev",
+      domain: "https://wired-adder-62.clerk.accounts.dev",
       applicationID: "convex",
     },
   ],
